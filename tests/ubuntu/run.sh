402eec4896be5a5d22109086568a8cae5b09acac
Killian KOPP <killiankopp@gmail.com>
2024-05-27 16:20:06 +0200

    installation de K3s

--- tests/ubuntu/run.sh
+++ tests/ubuntu/run.sh
@@ -1,3 +1,3 @@
 sudo docker build -t ubuntu_test .
-sudo docker run -d --name node1 ubuntu_test sleep infinity
+sudo docker run -d --name node1 --privileged -v /sys/fs/cgroup:/sys/fs/cgroup:ro ubuntu_test sleep infinity
 sudo docker exec -it node1 /bin/bash
